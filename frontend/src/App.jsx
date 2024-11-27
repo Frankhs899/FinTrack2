336f3c3da1f40a5769bf07bd01168cf126e0d13f
Franklin Hurtado Salazar <137368806+Frankhs899@users.noreply.github.com>
2024-11-26 22:41:57 -0500

    Defining page paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 
+import { HashRouter } from 'react-router-dom';
+
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import Container from './components/container/Container';
 
 function App() {
   return (
-    <div className='min-h-screen w-full flex flex-col bg-base-200 text-base-800 dark:bg-base-900 dark:text-base-100'>
-      <Navbar />
-      <Container />
-      <Footer />
-    </div>
+    <HashRouter>
+      <div className='min-h-screen w-full flex flex-col bg-base-200 text-base-800 dark:bg-base-900 dark:text-base-100'>
+        <Navbar />
+        <Container />
+        <Footer />
+      </div>
+    </HashRouter>
   );
 }
 
--- frontend/src/components/container/Container.jsx
+++ frontend/src/components/container/Container.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
+import { Routes, Route } from 'react-router-dom';
+import Home from '../../pages/home/Home';
+import Expense from '../../pages/expense/Expense';
+import Income from '../../pages/income/Income';
+
 function Container() {
-  return <main className='flex flex-grow min-h-full p-4'>App</main>;
+  return (
+    <main className='flex flex-grow min-h-full p-4'>
+      <Routes>
+        <Route
+          path='/'
+          element={<Home />}
+        />
+        <Route
+          path='/expense'
+          element={<Expense />}
+        />
+        <Route
+          path='/income'
+          element={<Income />}
+        />
+      </Routes>
+    </main>
+  );
 }
 
 export default Container;
