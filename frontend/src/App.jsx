2812a98357a6eeb91da972fdd2e3cc11ff6d7d2f
Franklin Hurtado Salazar <137368806+Frankhs899@users.noreply.github.com>
2024-11-26 22:20:17 -0500

    Creating route container

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
+import Container from './components/container/Container';
 
 function App() {
   return (
     <div className='min-h-screen w-full flex flex-col bg-base-200 text-base-800 dark:bg-base-900 dark:text-base-100'>
       <Navbar />
-      <div>App</div>
+      <Container />
       <Footer />
     </div>
   );
---
+++ frontend/src/components/container/Container.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Container() {
+  return <main className='flex flex-grow min-h-full p-4'>App</main>;
+}
+
+export default Container;
