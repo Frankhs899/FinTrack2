12e440e9d2d3020e82db9dcc1ee8cbc2e448a568
Franklin Hurtado Salazar <137368806+Frankhs899@users.noreply.github.com>
2024-11-29 20:35:52 -0500

    Creating component to render balance, importing Balance component to Home.

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Balance from './components/balance/Balance';
 
 function Home() {
   return (
     <div className='flex flex-col items-center w-full min-h-full gap-4 overflow-x-hidden lg:flex-row'>
+      <Balance />
       Home
     </div>
   );
