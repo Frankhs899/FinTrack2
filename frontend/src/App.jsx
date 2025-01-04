e1a7e557b7c10d07785b3ea12de08e51170cfc88
Franklin Hurtado Salazar <137368806+Frankhs899@users.noreply.github.com>
2025-01-04 01:10:54 -0500

    global context to manage the theme (light/dark mode)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import { useContext } from 'react';
+import { ThemeContext } from './context/ThemeContext';
 
 function App() {
-  return <div className='bg-primary-500 font-[Inter]'>App</div>;
+  const { theme, handleChangeTheme } = useContext(ThemeContext);
+  return (
+    <div className='bg-primary-500 dark:bg-secondary-500 font-[Inter]'>
+      <h1>App</h1>
+      <button onClick={handleChangeTheme}>Cambiar tema</button>
+    </div>
+  );
 }
 
 export default App;
